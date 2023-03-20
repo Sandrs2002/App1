d8fc292a9b095f374a3e724317ee691926c7345b
Sandrs2002 <aleksandra.anashkevich@gmail.com>
2023-03-20 09:49:52 +0200

    update

--- App1/App1/Lumememm.xaml.cs
+++ App1/App1/Lumememm.xaml.cs
@@ -2,15 +2,110 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace App1
 {	
 	public partial class Lumememm : ContentPage
-	{	
-		public Lumememm ()
+	{
+		BoxView bucket, snow1, snow2;
+        Slider sld;
+        public Lumememm ()
 		{
-			InitializeComponent ();
-		}
-	}
+            BackgroundImageSource = "snowmount.jpg";
+            bucket = new BoxView
+			{
+                BackgroundColor = Color.Black,
+                CornerRadius = 10,
+                WidthRequest = 60,
+                HeightRequest = 60,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Start
+        };
+            snow1 = new BoxView
+            {
+                BackgroundColor = Color.White,
+                CornerRadius = 75,
+                WidthRequest = 150,
+                HeightRequest = 150,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+            };
+            snow2 = new BoxView
+            {
+                BackgroundColor = Color.White,
+                CornerRadius = 100,
+                WidthRequest = 200,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.End,
+            };
+            Xamarin.Forms.Button seen = new Xamarin.Forms.Button
+            {
+                Text = "Show",
+                TextColor = Color.LimeGreen,
+                BackgroundColor = Color.White,
+            };
+            Xamarin.Forms.Button unseen = new Xamarin.Forms.Button
+            {
+                Text = "Hide",
+                TextColor = Color.LimeGreen,
+                BackgroundColor = Color.White,
+            };
+            Xamarin.Forms.Button color = new Xamarin.Forms.Button
+            {
+                Text = "Color ",
+                TextColor = Color.LimeGreen,
+                BackgroundColor = Color.White,
+            };
+           
+
+            AbsoluteLayout abs = new AbsoluteLayout
+            {
+                Children = { bucket, snow1, snow2, seen, unseen, color}
+            };
+            AbsoluteLayout.SetLayoutBounds(bucket, new Rectangle(0.5, 0.1, 60, 60));
+            AbsoluteLayout.SetLayoutFlags(bucket, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(snow1, new Rectangle(0.5, 0.2, 150, 150));
+            AbsoluteLayout.SetLayoutFlags(snow1, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(snow2, new Rectangle(0.5, 0.45, 200, 200));
+            AbsoluteLayout.SetLayoutFlags(snow2, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(seen, new Rectangle(0, 0.9, 400, 50));
+            AbsoluteLayout.SetLayoutFlags(seen, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(unseen, new Rectangle(0, 0.95, 400, 50));
+            AbsoluteLayout.SetLayoutFlags(unseen, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(color, new Rectangle(0, 1, 400, 50));
+            AbsoluteLayout.SetLayoutFlags(color, AbsoluteLayoutFlags.PositionProportional);
+            Content = abs;
+
+            seen.Clicked += Seen_Clicked;
+            unseen.Clicked += Unseen_Clicked;
+            color.Clicked += Color_Clicked;
+            
+        }
+
+        Random rnd;
+
+        private void Color_Clicked(object sender, EventArgs e)
+        {
+            rnd = new Random();
+            snow1.Color = Color.FromRgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            snow2.Color = Color.FromRgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+        }
+
+        private void Unseen_Clicked(object sender, EventArgs e)
+        {
+            bucket.IsVisible = false;
+            snow1.IsVisible = false;
+            snow2.IsVisible = false;
+        }
+
+        private void Seen_Clicked(object sender, EventArgs e)
+        {
+            bucket.IsVisible = true;
+            snow1.IsVisible = true;
+            snow2.IsVisible = true;
+        }
+    }
 }
 
--- App1/App1/StepperSlider_Page.xaml.cs
+++ App1/App1/StepperSlider_Page.xaml.cs
@@ -4,13 +4,58 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace App1
-{	
+{
 	public partial class StepperSlider_Page : ContentPage
-	{	
-		public StepperSlider_Page ()
+	{
+		Label lbl;
+		Slider sld;
+		Stepper stp;
+		public StepperSlider_Page()
 		{
-			InitializeComponent ();
+			lbl = new Label
+			{
+				Text = "...",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.CenterAndExpand
+			};
+			sld = new Slider
+			{
+				Minimum = 0,
+				Maximum = 100,
+				Value = 30,
+				MinimumTrackColor = Color.White,
+				MaximumTrackColor = Color.Black,
+				ThumbColor = Color.Purple,
+			};
+			sld.ValueChanged += Sld_ValueChanged;
+			stp = new Stepper
+			{
+				Minimum = 0,
+				Maximum = 100,
+				Increment = 1,
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.CenterAndExpand
+			};
+			stp.ValueChanged += Stp_ValueChanged;
+			StackLayout st = new StackLayout
+			{
+				Children = {sld, lbl, stp}
+			};
+			Content = st;
+		}
+
+		private void Stp_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			lbl.Text = String.Format("Valitud: {0:F1}", e.NewValue);
+			lbl.FontSize = e.NewValue;
+			lbl.Rotation = e.NewValue;
+		}
+
+		private void Sld_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			lbl.Text = String.Format("Valitud: {0:F1}", e.NewValue);
+			lbl.FontSize = e.NewValue;
+			lbl.Rotation = e.NewValue;
 		}
 	}
 }
-
